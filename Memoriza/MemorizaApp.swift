a5dea0d6adea5ec77a3c13328114ccaec5b4d157
dev_ElderLab <enderlabo@gmail.com>
2021-03-14 15:49:12 -0700

    Initial Commit

---
+++ Memoriza/MemorizaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizaApp.swift
+//  Memoriza
+//
+//  Created by Laborit on 14/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
